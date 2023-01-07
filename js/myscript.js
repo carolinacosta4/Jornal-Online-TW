606a9bba15da3c309c66e790a111155c6f0d8718
Carolina Costa <carolinamccosta@gmail.com>
2023-01-07 23:52:05 +0000

    updates

--- js/myscript.js
+++ js/myscript.js
@@ -20,3 +20,47 @@ function carousel() {
   x[slideIndex-1].style.display = "block"; 
   setTimeout(carousel, 2000); 
 }
+
+function myFunction() {
+  document.getElementById("like").style.color = "#999999";
+}
+
+function CopyText() {
+  var copyText = document.getElementById("link");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); 
+
+  navigator.clipboard.writeText(copyText.value);
+
+  alert("Link copiado!");
+}
+
+function initPayPalButton() {
+  paypal.Buttons({
+  style: {
+      shape: 'rect',
+      color: 'gold',
+      layout: 'vertical',
+      label: 'paypal',
+      
+  },
+
+  createOrder: function(data, actions) {
+      return actions.order.create({
+      purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+      });
+  },
+
+  onApprove: function(data, actions) {
+      return actions.order.capture().then(function(details) {
+      alert('Transaction completed by ' + details.payer.name.given_name + '!');
+      });
+  },
+
+  onError: function(err) {
+      console.log(err);
+  }
+  }).render('#paypal-button-container');
+}
+initPayPalButton();
